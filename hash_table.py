3595a6f475d79f197c1b77828f1eba5763b7b57b
Stephen Bell <stephen.l.bell@gmail.com>
2020-08-29 18:17:57 -0400

    Stub out methods for hash_table class

---
+++ hash_table.py
@@ -0,0 +1,39 @@
+import random as r
+
+# the number of bytes in an integer
+w = 32
+
+
+class HashTable(object):
+    def __init__(self, initial_capacity=10):
+        self.__initialize()
+
+    def __initialize(self):
+        pass
+
+    def add(self, val):
+        pass
+
+    def remove(self, val):
+        pass
+
+    def find(self, val):
+        pass
+
+    def clear(self):
+        pass
+
+    def __resize(self):
+        pass
+
+    def __hash(self, val):
+        pass
+
+    def __allocate_backing_array(self, size):
+        pass
+
+    def __random_odd_int(self):
+        pass
+
+    def __iter__(self):
+        pass
