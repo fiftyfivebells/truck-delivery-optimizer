bf43102557ba28be0033ddbcccbdff6ded1eefba
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 15:36:54 -0400

    Create skeleton of the Location class

---
+++ Location.py
@@ -0,0 +1,16 @@
+class Location:
+    def __init__(self):
+        pass
+
+    def __hash__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+        
