f8e1c289c539289625092e46896c2ff35bf6aef2
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 16:07:40 -0400

    Create helper methods for priority queue implementation

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -3,9 +3,22 @@ class PQItem(object):
         self.item = item
         self.priority = priority
 
+# Returns the left child of given index
+def left(i):
+    return 2*i + 1
+
+# Returns right child of given index
+def right(i):
+    return 2*(i + 1)
+
+# Returns parent of given index
+def parent(i):
+    return (i - 1) // 2
+
+
 class PriorityQueue(object):
     def __init__(self):
-        pass
+        
 
     def is_empty(self):
         pass
