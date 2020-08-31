674e5a89730261dab763e1388f1c7bbf6f9d782e
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 15:34:12 -0400

    Implement methods for the Package class

--- Package.py
+++ Package.py
@@ -1,9 +1,13 @@
 class Package:
-    def __init__(self):
-        pass
+    def __init__(self, package_id, address, deadline, weight):
+        self.package_id = package_id
+        self.address = address
+        self.deadline = deadline
+        self.weight = weight
+        self.is_delivered = False
 
     def __eq__(self, other):
-        pass
+        return self.package_id == other.package_id
 
     def __hash__(self):
-        pass
+        return hash(self.package_id)
