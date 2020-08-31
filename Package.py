587c1d1ad36973e945378df98e063b51077c9167
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 16:04:52 -0400

    Add attribute for distance of package address from hub

--- Package.py
+++ Package.py
@@ -1,10 +1,11 @@
 class Package:
-    def __init__(self, package_id, address, deadline, weight):
+    def __init__(self, package_id, address, deadline, weight, distance):
         self.package_id = package_id
         self.address = address
         self.deadline = deadline
         self.weight = weight
         self.is_delivered = False
+        self.distance_from_hub = distance
 
     def __eq__(self, other):
         return self.package_id == other.package_id
