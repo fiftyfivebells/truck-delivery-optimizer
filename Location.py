36f1434156438d8002149fded7e3c1c69dfc1eb8
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 15:48:03 -0400

    Implement the methods for Location

--- Location.py
+++ Location.py
@@ -1,16 +1,23 @@
 class Location:
-    def __init__(self):
-        pass
+    def __init__(self, name, street, city, state, zip_code):
+        self.name = name
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
 
     def __hash__(self):
-        pass
+        return hash(self.street, self.city, self.name)
 
     def __eq__(self, other):
-        pass
+        if other == None:
+            return False
+        elif isinstance(other, Location):
+            return self.street == other.street and self.city == other.city and self.zip_code == other.zip_code
 
     def __str__(self):
-        pass
+        return f"{self.street}, {self.city}, {self.state} {self.zip_code}"
 
     def __repr__(self):
-        pass
+        return f"Location({self.name}, {self.street}, {self.city}, {self.state}, {self.zip_code})"
         
