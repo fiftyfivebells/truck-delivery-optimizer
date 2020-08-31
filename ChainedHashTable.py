b636915d885c9eab6f0a516c9f19549e1e32852c
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-30 20:12:11 -0400

    Update some methods to more closely match Python dicts

--- ChainedHashTable.py
+++ ChainedHashTable.py
@@ -22,14 +22,23 @@ class HashTable(object):
         if self.size + 1 > len(self.array):
             self.__resize()
 
-        self.array[self.__hash(val)].append(val)
+        # if the key already exists, get rid of it before inserting a new one
+        if self.find(key) is not None:
+            self.array[self.__hash(key)].remove((key, val))
+
+        self.array[self.__hash(key)].append((key, val))
         self.size += 1
 
     # Updates the value associated with the given key to the new value. Operates in O(1) time, since the
     # size of the array at each index in the backing array is very small (since the hashing function is good)
+    # Removes the item with the specified key in amortized O(1) time. Each call to resize is O(n), but 
+    # reize should only be called occasionally, allowing the cost to be amortized. Returns None if the
+    # key is not in the list.
+    def remove(self, key):
+        elements = self.array[self.__hash(key)]
         for x in elements:
-            if x == val:
-                elements.remove(val)
+            if x[0] == key:
+                elements.remove(x)
                 self.size -= 1
                 if 3 * self.size < len(self.array):
                     self.__resize()
@@ -39,6 +48,12 @@ class HashTable(object):
     # Finds and returns the value associated with the given key, or None if the key doesn't exist. This
     # is O(1) in practice, because even though we iterate the list at each index, the number of items in
     # the list is very small. Raises a KeyError if the key doesn't exist
+    def find(self, key):
+        for x in self.array[self.__hash(key)]:
+            if x[0] == key:
+                return x[1]
+        raise KeyError(key)
+
 
     # O(n) time. Sets the dimension back to 1, creates a new backing array of size 2, and then sets the
     # number of items currently in the list back to 0
@@ -62,7 +77,7 @@ class HashTable(object):
 
         for i in range(len(old_array)):
             for x in old_array[i]:
-                self.add(x)
+                self.insert(x[0], x[1])
 
     # Generates a hash value in O(1) time. First it creates a hash from the given value by multiplying it by a
     # random 32 bit number, the getting the remainder of that divided by 2**32 (to throw away some of the bits),
@@ -70,8 +85,7 @@ class HashTable(object):
     # of the backing array. The function is done again using the hashed value in place of the original value
     # to reduce collisions.
     def __hash(self, val):
-        hashed_val = ((self.seed * int(val)) % 2 ** w) // 2 ** (w - self.dimension)
-        return ((self.seed * hashed_val) % 2 ** w) // 2 ** (w - self.dimension)
+        return ((self.seed * hash(val)) % 2 ** w) // 2 ** (w - self.dimension)
 
     # Creates a new backing array in O(n) time. This simply takes in a size value, then creates a list of
     # size "size" with an empty list at each index. This is linear time because it goes from index 0 to
@@ -85,7 +99,9 @@ class HashTable(object):
     def __random_odd_int(self):
         return r.randrange(2 ** w) | 1  # gets random int in range 1 - 2^32, then ORs it with 1 to make it odd
 
+    # Allows users to iterate over the hash table with for loops. This is an O(n) time operation because it
+    # touches every item in the list once in order to iterate everything it contains
     def __iter__(self):
         for elements in self.array:
             for x in elements:
-                yield x
+                yield x[0], x[1]
