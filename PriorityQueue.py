8a09f8a6a4b958966aff979f61a7b541f6d0c22f
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 16:04:34 -0400

    Make skeleton for priority queue class

---
+++ PriorityQueue.py
@@ -0,0 +1,19 @@
+class PQItem(object):
+    def __init__(self, item, priority):
+        pass
+
+class PriorityQueue(object):
+    def __init__(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
