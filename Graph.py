8952d2e01ef18a6daf0952fce8ae0e1a6930e1cd
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 15:08:23 -0400

    Implement method to get closest unvisited node from the source node

--- Graph.py
+++ Graph.py
@@ -19,5 +19,15 @@ class Graph:
     def get_distance_between(self, u, v):
         return self.graph[u][v]
 
-    def shortest_distance(self, start):
-        pass
+    # Finds the vertex with the smallest distance value from the set of vertices
+    # that are not yet included in the shortest path table
+    def __min_distance(self, distance, visited):
+        min = float('inf')
+
+        for v in range(self.vertices):
+            if distance[v] < min and visited[v] == False:
+                min = distance[v]
+                min_index = v
+                
+        return min_index
+
