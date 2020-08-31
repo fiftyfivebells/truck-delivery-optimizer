80636a77ee4821d50ea625f10b70879033153289
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 13:06:14 -0400

    Create skeleton of Graph class

---
+++ Graph.py
@@ -0,0 +1,12 @@
+class Graph:
+    def __init__(self, vertices):
+        pass
+
+    def add_edge(self, u, v, weight):
+        pass
+
+    def get_distance_between(self, u, v):
+        pass
+
+    def shortest_distance(self, start):
+        pass
