b7493b3641afefb2e8a5f0fb57e5c774eb952514
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 15:07:35 -0400

    Implement the __init__ method

--- Graph.py
+++ Graph.py
@@ -1,6 +1,12 @@
 class Graph:
     def __init__(self, vertices):
-        pass
+        # creates a 2d list of vertices, where each entry in the table is a
+        # weighted edge between the row vertex and the column vertex
+        self.graph = [[0 for col in range(vertices)]
+                      for row in range(vertices)]
+
+        # total number of vertices in graph
+        self.vertices = vertices 
 
     def add_edge(self, u, v, weight):
         pass
