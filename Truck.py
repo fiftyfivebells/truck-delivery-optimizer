bb279c3f1e1b837c131b677d6f859350d6325f64
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 21:08:00 -0400

    Implement first pass at a shortest route algorithm for the truck

--- Truck.py
+++ Truck.py
@@ -30,3 +30,41 @@ class Truck(object):
     def is_truck_full(self):
         return len(self.packages) == self.capacity
 
+    def find_shortest_route(self):
+        route = [self.start]
+        current = self.start
+
+        locs = list(self.location_list)
+
+        while len(locs) > 0:
+            nearest = None
+            shortest = None
+
+            nearest_idx = 0
+            index = 0
+
+            for loc in locs:
+                u = current.graph_index
+                v = loc.graph_index
+                closest = self.distances.get_distance_between(u, v)
+
+                if shortest == None or shortest > closest:
+                    shortest = closest
+                    nearest = loc
+                    nearest_idx = index
+                index += 1
+
+            route.append(locs.pop(nearest_idx))
+            current = nearest
+
+        route.append(self.start)
+
+        previous = None
+        cost = 0.0
+        for stop in route:
+            if previous is not None:
+                cost = round(cost + self.distances.get_distance_between(previous.graph_index, stop.graph_index), 2)
+
+            previous = stop
+
+        return (cost, route)
