6f1ae533f682d37d259db654674b030aae698fad
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 20:15:31 -0400

    Create skeleton of Truck class

---
+++ Truck.py
@@ -0,0 +1,12 @@
+class Truck(object):
+    def __init__(self):
+        pass
+
+    def load_truck(self):
+        pass
+
+    def start_deliveries(self):
+        pass
+
+    def is_truck_full(self):
+        pass
