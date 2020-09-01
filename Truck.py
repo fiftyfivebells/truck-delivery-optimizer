32a55c89938c211eb23adfece8a9cbac1aebf6cd
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 20:18:28 -0400

    Implement first pass at the init method

--- Truck.py
+++ Truck.py
@@ -1,6 +1,16 @@
 class Truck(object):
-    def __init__(self):
-        pass
+    def __init__(self, id, distances, start, avg_speed, capacity):
+        self.id = id
+        self.distances = distances
+        self.start = start
+        self.avg_speed = avg_speed
+        self.capacity = capacity
+
+        self.location = start
+        self.route = []
+        self.packages = []
+        self.total_distance = 0.0
+        self.current_time = None
 
     def load_truck(self):
         pass
