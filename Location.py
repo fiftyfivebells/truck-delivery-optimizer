89a66c83e2a2761dff70774da9e1a6d598b0b5b2
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 20:11:36 -0400

    Add graph_index to the location class

--- Location.py
+++ Location.py
@@ -1,10 +1,11 @@
 class Location:
-    def __init__(self, name, street, city, state, zip_code):
+    def __init__(self, name, street, city, state, zip_code, graph_index):
         self.name = name
         self.street = street
         self.city = city
         self.state = state
         self.zip_code = zip_code
+        self.graph_index = graph_index
 
     def __hash__(self):
         return hash(self.street, self.city, self.name)
