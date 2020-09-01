22338db234f54e0b8f6a8f84c80d8a8b8add6ecd
fiftyfivebells <stephen.l.bell@gmail.com>
2020-08-31 20:12:01 -0400

    Remove distance from constructor and initialize to infinity

--- Package.py
+++ Package.py
@@ -1,11 +1,11 @@
 class Package:
-    def __init__(self, package_id, address, deadline, weight, distance):
+    def __init__(self, package_id, address, deadline, weight):
         self.package_id = package_id
         self.address = address
         self.deadline = deadline
         self.weight = weight
         self.is_delivered = False
-        self.distance_from_hub = distance
+        self.distance_from_hub = float('inf')
 
     def __eq__(self, other):
         return self.package_id == other.package_id
