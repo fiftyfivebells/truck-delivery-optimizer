d225da67caf1a8b14481ea3605b83d485a6cf5fa
fiftyfivebells <stephen.l.bell@gmail.com>
2020-09-02 18:11:23 -0400

    Implement method to load truck from a list of package ids

--- WGUPS.py
+++ WGUPS.py
@@ -3,6 +3,13 @@ from Truck import Truck
 from PriorityQueue import PriorityQueue
 from WGU_Time import Time
 
+def load_truck_from_list(truck, special, packages):
+    for p in special:
+        if not truck.is_truck_full():
+            truck.load_truck(packages[p])
+            packages[p].truck_id = truck.id
+    
+
 class WGUPS(object):
     def __init__(self):
         pass
